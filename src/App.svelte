7fe860bd50bfd0f048c276bee8da1767a948b0a1
Davi de Carvalho <davi_carvalho96@hotmail.com>
2019-09-07 10:58:11 -0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { Router, Link, Route } from 'svelte-routing';
+    import Patients from './pages/patients/Patients.svelte';
+
+    let url = '';
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<Router url="{url}">
+    <Route path="patients" component="{Patients}" />
+</Router>
---
+++ src/pages/patients/Patients.svelte
@@ -0,0 +1,29 @@
+<script>
+	let patientListPromise = getPatientList();
+
+	async function getPatientList() {
+	  const response = await fetch("http://localhost:8080/api/v1/pacient");
+	  return await response.json();
+	}
+</script>
+
+<h1>To aqui na tela de pacientes</h1>
+
+<div>
+
+{#await patientListPromise}
+
+    <p>Loading patients</p>
+
+    {:then patientList}
+
+        {#each patientList as patient (patient.id)}
+            <p>{patient.name}</p>
+        {/each}
+
+    {:catch error}
+
+        <p style="color: red">{error}</p>
+
+{/await}
+</div>
